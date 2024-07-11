ff5d2b7676d03b1c122d2c46b258b00e75c020f9
williamtuominiemi <william.m.tuominiemi@gmail.com>
2024-07-11 20:03:41 +0300

    2 new level designs

--- utils/Maps.ts
+++ utils/Maps.ts
@@ -11,22 +11,24 @@ const level1 = [
 ];
 
 const level2 = [
+  new THREE.Vector2(10, 0),
   new THREE.Vector2(0, 0),
-  new THREE.Vector2(15, 0),
-  new THREE.Vector2(15, 30),
-  new THREE.Vector2(35, 30),
-  new THREE.Vector2(35, 45),
-  new THREE.Vector2(0, 45),
-  new THREE.Vector2(0, 0),
+  new THREE.Vector2(5, 45),
+  new THREE.Vector2(-40, 40),
+  new THREE.Vector2(-40, 50),
+  new THREE.Vector2(10, 50),
+  new THREE.Vector2(10, 0),
 ];
 
 const level3 = [
   new THREE.Vector2(0, 0),
-  new THREE.Vector2(20, 0),
-  new THREE.Vector2(20, 20),
-  new THREE.Vector2(30, 20),
-  new THREE.Vector2(30, 30),
-  new THREE.Vector2(0, 30),
+  new THREE.Vector2(12, 0),
+  new THREE.Vector2(20, 40),
+  new THREE.Vector2(12, 80),
+  new THREE.Vector2(20, 120),
+  new THREE.Vector2(12, 120),
+  new THREE.Vector2(0, 80),
+  new THREE.Vector2(12, 40),
   new THREE.Vector2(0, 0),
 ];
 
